f56ca4cb4b02b395886b324e760c9efe7a083455
PsammystPrime <samuelprime384@gmail.com>
2024-03-29 14:15:42 +0300

    Add dares functionality

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const dare = document.querySelector(".dare");
 const btns = document.querySelector(".btns");
 
 truth.addEventListener("click", truths);
-// dare.addEventListener("click", dares);
+dare.addEventListener("click", dares);
 
-score = 1;
+score = 0;
 
 function truths() {
   const truthsArray = [
@@ -19,12 +19,23 @@ function truths() {
   ];
   const choice = Math.floor(Math.random() * truthsArray.length);
   quiz.textContent = truthsArray[choice];
-  decisions();
-  //   console.log("aa");
-  //   console.log();
+  truthSelection();
 }
 
-function decisions() {
+function dares() {
+  const daresArray = [
+    "Kiss your ankle?",
+    "Give me a dollar?",
+    "Stand with one hand?",
+    "lick your nose",
+    "Rotate both hands in opposite directions",
+  ];
+  const choice = Math.floor(Math.random() * daresArray.length);
+  quiz.textContent = daresArray[choice];
+  dareSelection();
+}
+
+function truthSelection() {
   const done = document.createElement("button");
   const tryAgain = document.createElement("button");
 
@@ -36,24 +47,59 @@ function decisions() {
   btns.appendChild(tryAgain);
 
   done.addEventListener("click", addScore);
-  //   tryAgain.addEventListener("click", removeScore);
+  tryAgain.addEventListener("click", removeScore);
+}
 
-  function addScore() {
-    scores.textContent = "Score: " + score++;
-    quiz.textContent = "Congrats!! Let's go again";
-    const truth = document.createElement("button");
-    const dare = document.createElement("button");
+function dareSelection() {
+  const done = document.createElement("button");
+  const tryAgain = document.createElement("button");
 
-    truth.className = "truth";
-    dare.className = "dare";
-    truth.textContent = "Truth";
-    dare.textContent = "Dare";
+  done.textContent = "Done";
+  tryAgain.textContent = "Choose again?";
 
-    truth.addEventListener("click", truths);
-    // dare.addEventListener("click", dares);
+  btns.textContent = "";
+  btns.appendChild(done);
+  btns.appendChild(tryAgain);
 
-    btns.textContent = "";
-    btns.appendChild(truth);
-    btns.appendChild(dare);
-  }
+  done.addEventListener("click", addScore);
+  tryAgain.addEventListener("click", removeScore);
+}
+
+function addScore() {
+  scores.textContent = "Score: " + (score++ + 1);
+  quiz.textContent = "Congrats!! Let's go again";
+  const truth = document.createElement("button");
+  const dare = document.createElement("button");
+
+  truth.className = "truth";
+  dare.className = "dare";
+  truth.textContent = "Truth";
+  dare.textContent = "Dare";
+
+  truth.addEventListener("click", truths);
+  dare.addEventListener("click", dares);
+
+  btns.textContent = "";
+  btns.appendChild(truth);
+  btns.appendChild(dare);
+}
+
+function removeScore() {
+  scores.textContent = "Score: " + (score-- - 1);
+
+  quiz.textContent = "OOoops!! Let's go again";
+  const truth = document.createElement("button");
+  const dare = document.createElement("button");
+
+  truth.className = "truth";
+  dare.className = "dare";
+  truth.textContent = "Truth";
+  dare.textContent = "Dare";
+
+  truth.addEventListener("click", truths);
+  dare.addEventListener("click", dares);
+
+  btns.textContent = "";
+  btns.appendChild(truth);
+  btns.appendChild(dare);
 }
