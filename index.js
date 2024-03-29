937269d568d000ecf15b46cac5ff9a77fa354bb1
PsammystPrime <samuelprime384@gmail.com>
2024-03-29 14:57:26 +0300

    Refactor selection buttons to avoid repetition

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function truths() {
   ];
   const choice = Math.floor(Math.random() * truthsArray.length);
   quiz.textContent = truthsArray[choice];
-  truthSelection();
+  choiceButtons();
 }
 
 function dares() {
@@ -32,25 +32,10 @@ function dares() {
   ];
   const choice = Math.floor(Math.random() * daresArray.length);
   quiz.textContent = daresArray[choice];
-  dareSelection();
+  choiceButtons();
 }
 
-function truthSelection() {
-  const done = document.createElement("button");
-  const tryAgain = document.createElement("button");
-
-  done.textContent = "Done";
-  tryAgain.textContent = "Choose again?";
-
-  btns.textContent = "";
-  btns.appendChild(done);
-  btns.appendChild(tryAgain);
-
-  done.addEventListener("click", addScore);
-  tryAgain.addEventListener("click", removeScore);
-}
-
-function dareSelection() {
+function choiceButtons() {
   const done = document.createElement("button");
   const tryAgain = document.createElement("button");
 
@@ -69,7 +54,7 @@ function addScore() {
   scores.textContent = "Score: " + (score++ + 1);
   quiz.textContent = "Congrats!! Let's go again";
 
-  navButtons;
+  homeButtons;
 
   btns.textContent = "";
   btns.appendChild(truth);
@@ -80,13 +65,14 @@ function removeScore() {
   scores.textContent = "Score: " + (score-- - 1);
   quiz.textContent = "Ooops!! Let's go again";
 
-  navButtons;
+  homeButtons;
+
   btns.textContent = "";
   btns.appendChild(truth);
   btns.appendChild(dare);
 }
 
-function navButtons() {
+function homeButtons() {
   const truth = document.createElement("button");
   const dare = document.createElement("button");
 
